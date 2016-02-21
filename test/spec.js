7ea7734425f9801f4e4ae79d5553a79cdf6d4cfe
rtatol <rtatol@gmail.com>
2016-02-21 22:02:45 +0100

    added integration test and dependencies

---
+++ test/spec.js
@@ -0,0 +1,25 @@
+var request = require('supertest');
+
+describe('app tests', function () {
+    var server;
+
+    beforeEach(function () {
+        server = require('../app');
+    });
+
+    it('should responds to /', function testHome(done) {
+        request(server)
+            .get('/')
+            .expect(200, done);
+    });
+
+    it('should throw 404 when everything else', function test404(done) {
+        request(server)
+            .get('/foo/bar')
+            .expect(404, done);
+    });
+
+    afterEach(function (done) {
+        done();
+    });
+});
