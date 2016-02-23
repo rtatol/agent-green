941ae51fcfbc588c96fc911cdae77dc5dcd774a6
rtatol <rtatol@gmail.com>
2016-02-24 00:20:41 +0100

    added view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+var sensors = require('../sensors/sensorReader');
+
+router.get('/', function (req, res) {
+
+    var ds18b20 = sensors.read('DS18B20');
+    var cpuTemp = sensors.read('CPU_SENSOR');
+
+    res.render('index', {
+        title: 'agent green',
+        currentDate: new Date(),
+        ds18b20Value: ds18b20,
+        cpuTempValue: cpuTemp
+    });
 });
 
 module.exports = router;
